063d8714cecdadcf813a17c1a1ad6391d59ab792
tsunodajapa <gabrieltsunoda@gmail.com>
2023-10-20 07:45:18 -0300

    initial commit

--- next.config.js
+++ next.config.js
@@ -1,4 +1,33 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {}
+const nextConfig = {
+  async rewrites() {
+    return [
+        {
+            source: "/dashServicos/:path*",
+            destination: `http://erpbuybyehomolog.methodostecnologia/dashServicos/:path*`,
+        },
+        {
 
-module.exports = nextConfig
+            source: "/views/:path*",
+            destination: `http://erpbuybyehomolog.methodostecnologia/views/:path*`,
+        },
+        {
+
+            source: "/plugins/:path*",
+            destination: `http://erpbuybyehomolog.methodostecnologia/plugins/:path*`,
+        },
+        {
+
+            source: "/permissoes/:path*",
+            destination: `http://erpbuybyehomolog.methodostecnologia/permissoes/:path*`,
+        },
+        {
+
+            source: "/v2/:path*",
+            destination: `http://erpbuybyehomolog.methodostecnologia/v2/:path*`,
+        },
+    ]    
+  },
+};
+
+module.exports = nextConfig;
--- src/app/outro/page.tsx
+++ src/app/outro/page.tsx
@@ -4,16 +4,10 @@ import Link from 'next/link'
 export default function Home() {
   return (
     <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      <h1>Teste</h1>
+      Outro
 
-      <Link href="/teste">
-        Para Projeto JS
-      </Link>
-      <Link href="/dashServicos/servicosHome">
-        Para Projeto JS
-      </Link>
-      <Link href="/outro">
-        Outro
+      <Link href="/">
+        Voltar
       </Link>
     </main>
   )
